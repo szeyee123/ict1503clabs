dd4686b30a3a5f89cabee9a35a99a367d14281b2
szeyee <szeyee.tew@the-elxr.com>
2024-10-05 23:31:40 +0800

    Lab3Assignment

--- Lab3Assignment/guessword.c
+++ Lab3Assignment/guessword.c
@@ -4,6 +4,9 @@
 #include <ctype.h>
 #include <string.h>
 
+#define MAX_WORD_LENGTH 7
+#define MAX_INCORRECT_GUESSES 7
+
 void to_lowercase(char *str) {
     for (int i = 0; str[i] != '\0'; i++) {
         str[i] = tolower(str[i]);
@@ -11,13 +14,12 @@ void to_lowercase(char *str) {
 }
 
 int is_word(const char *word) {
-    if (strlen(word) > 7) {
-        return 0;
-    } // Check if word length is more than 7
-    
+    if (strlen(word) > MAX_WORD_LENGTH) {
+        return 0; // Check if word length is more than 7
+    }
     for (int i = 0; word[i] != '\0'; i++) {
         if (!isalpha(word[i])) {
-            return 0; // If input includes non letters
+            return 0; // If input includes non-letters
         }
     }
     return 1; 
@@ -61,10 +63,20 @@ int word_guessed(const char *current_guess) {
     return 1; // Word has been guessed
 }
 
+int already_guessed(char letter, const char *guessed_letters) {
+    for (int i = 0; guessed_letters[i] != '\0'; i++) {
+        if (guessed_letters[i] == letter) {
+            return 1; // Letter has already been guessed
+        }
+    }
+    return 0; // Letter has not been guessed
+}
+
 int main() {
-    char word[7 + 1], current_guess[7 + 1];
+    char word[MAX_WORD_LENGTH + 1], current_guess[MAX_WORD_LENGTH + 1];
     char guess;
     int incorrect_guesses = 0;
+    char guessed_letters[27] = ""; // Guessed letters (26 + 1 for null terminator)
 
     // Player 1 enters a word
     do {
@@ -72,39 +84,53 @@ int main() {
         scanf("%s", word);
         to_lowercase(word);
         if (!is_word(word)) {
-            printf("Sorry, the word must contain only English letters.\n");
+            printf("Sorry, the word must contain only English letters and be 7 letters or less.\n");
         }
     } while (!is_word(word));
 
-    int word_len = strlen(word);
     initialize_current_guess(word, current_guess);
 
     // Player 2 guessing
-    while (incorrect_guesses < 7) {
+    while (incorrect_guesses < MAX_INCORRECT_GUESSES) {
         display_current_guess(current_guess);
 
         // Check if the word has been guessed
         if (word_guessed(current_guess)) {
-            printf("Player 2 wins.");
+            printf("Player 2 wins!\n");
             return 0;
         }
 
-        printf("Player 2, you have %d guesses remaining. Enter your next guess:\n", 7 - incorrect_guesses);
-        
-        scanf(" %c", &guess);
-        guess = tolower(guess);
+        printf("Player 2, you have %d guesses remaining. Enter your next guess:\n", MAX_INCORRECT_GUESSES - incorrect_guesses);
 
-        // Check if letter is in word
+        char input[10];  // Buffer to read a string input
+        scanf("%s", input);
+        
+        // If the input length is greater than 1, count only the first letter
+        guess = tolower(input[0]);
+        
+        // Check if letter has already been guessed
+        if (already_guessed(guess, guessed_letters)) {
+            if (!letter_in_word(guess, word)) {
+                incorrect_guesses++;
+            } 
+            continue;
+        }
+
+        // Add guessed letter to the list of guessed letters
+        int len = strlen(guessed_letters);
+        guessed_letters[len] = guess;
+        guessed_letters[len + 1] = '\0';
+
+        // Check if letter is in the word
         if (letter_in_word(guess, word)) {
             reveal_letter_in_word(guess, word, current_guess);
         } else {
             incorrect_guesses++;
         }
-        
     }
 
     // Player 1 wins if Player 2 exceeds max number of incorrect guesses
     display_current_guess(current_guess);
-    printf("Player 2 has failed to guess the word %s. Player 1 wins.\n", word);
+    printf("Player 2 has failed to guess the word '%s'. Player 1 wins.\n", word);
     return 0;
 }
