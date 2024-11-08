05bd42d6652ab0ce132d42c881e324eb71dd8d8c
szeyee <szeyee.tew@the-elxr.com>
2024-11-09 01:13:13 +0800

    Lab 5 assignment

--- Lab5Assignment/minitar.c
+++ Lab5Assignment/minitar.c
@@ -53,7 +53,6 @@ void write_file_to_archive(FILE* output_file, const char* filename) {
     // Write file size (zero-padded)
     mini_header header;
     snprintf(header.size, SIZELEN, "%11d", file_size);
-    header.size[11] = '\0';
     fwrite(header.size, sizeof(char), SIZELEN, output_file);
 
     // Copy file content to archive
